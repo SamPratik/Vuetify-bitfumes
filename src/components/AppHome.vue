70eb067c8468ab31d60261d338ee565b5d1d12ac
SamPratik <pratik.anwar@gmail.com>
2018-05-11 02:33:57 +0600

    AppHome structure is ready

---
+++ src/components/AppHome.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<app-toolbar></app-toolbar>
+		<app-footer></app-footer>
+	</div>
+</template>
+
+<script>
+	import AppToolbar from './AppToolbar.vue'
+	import AppFooter from './AppFooter.vue'
+
+	export default {
+		components: {
+			AppToolbar,
+			AppFooter
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-toolbar color="indigo" dark>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title class="white--text">TheSoftKing</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>About</v-btn>
+      <v-btn flat>Contact</v-btn>
+      <v-btn flat>Login</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
