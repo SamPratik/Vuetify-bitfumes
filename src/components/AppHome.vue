9fa49fd862582ae023be3fdefe3d1511a1edec5e
SamPratik <pratik.anwar@gmail.com>
2018-05-11 23:52:16 +0600

    Carousel added to home page

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,15 +1,32 @@
 <template>
-	<div>
-		<h1>Home Component</h1>
-	</div>
+  <v-carousel>
+    <v-carousel-item v-for="(item,i) in items" :src="item.src" :key="i"></v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
-	export default {
-
-	}
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://vuetifyjs.com/static/doc-images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://vuetifyjs.com/static/doc-images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://vuetifyjs.com/static/doc-images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://vuetifyjs.com/static/doc-images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
+  }
 </script>
 
 <style scoped>
-	
+
 </style>
